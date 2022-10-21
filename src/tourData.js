1cd0bd5850cdd1d70a26e43f2164ff1e16eb54e9
Süleyman ÖZDAMAR <101826875+SuleymanOzdamar@users.noreply.github.com>
2022-10-21 18:51:47 +0300

    first

---
+++ src/tourData.js
@@ -0,0 +1,42 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpg",
+    name: "new york city tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpg",
+    name: "paris eyfel tower tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpg",
+    name: "london city tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpg",
+    name: "tokyo city tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 5,
+    city: "istanbul",
+    img: "./img/istanbul.jpg",
+    name: "istanbul city tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  }
+];
