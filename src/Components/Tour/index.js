ea03e34f861555801be317e83501bd1e6c797276
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2022-10-21 21:06:13 +0300

    update

--- src/Components/Tour/index.js
+++ src/Components/Tour/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import "./style.scss"
 
 class Tour extends Component {
+  
   render() {
+    const {id,city,img,name,info}=this.props.tour;
+    const {removeTour} = this.props;
     return (
-      <div>
+      <article className='tour'>
+        <div className='img-container'>
+          <img src={img} alt=''/>
+          <span className="close-btn" >
+            <i className='fas fa-window-close'></i>
+          </span>
+
+        </div>
+        <div className='tour-info'>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          info
+          <span>
+            <i className='fas fa-caret-square-down'></i>
+          </span>
+        </div>
         
-      </div>
+      </article>
     )
   }
 }
